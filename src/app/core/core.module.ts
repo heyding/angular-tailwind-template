5309ef09ac8fda2709e014a8ac940c784d5302d5
Tommy Heyding <toh@hbt.de>
2022-04-06 17:37:26 +0200

    Substitute hard coded text with i18n placeholders

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import {TranslateModule} from '@ngx-translate/core';
 
 
 
@@ -15,7 +16,8 @@ import { HeaderComponent } from './header/header.component';
     HeaderComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    TranslateModule
   ]
 })
 export class CoreModule { }
