72401e0f9eae17188c9d1424c56922da6caf2b90
Tommy Heyding <toh@hbt.de>
2022-04-06 10:31:43 +0200

    Modify tailwind.config.js to purge additional css and import 'Heebo' font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 module.exports = {
+  mode: 'jit',
+  purge: {
+    enabled: true,
+    content: ['./src/**/*.{html,ts}']
+  },
   content: [],
   theme: {
+    fontFamily: {
+      sans: ['Heebo', 'sans-serif']
+    },
     extend: {},
   },
   plugins: [],
