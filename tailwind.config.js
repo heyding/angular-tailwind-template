7bef4dafd0032079494011b6363c47772020b9df
Tommy Heyding <toh@hbt.de>
2022-04-06 10:33:23 +0200

    Add common tailwind plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,5 +11,9 @@ module.exports = {
     },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio')
+  ],
 }
