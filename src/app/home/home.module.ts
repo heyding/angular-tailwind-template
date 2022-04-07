72fafd9e34ca389f0287979d9e13f351b4ba6a89
Tommy Heyding <toh@hbt.de>
2022-04-07 09:22:08 +0200

    Add component that displays a 404 error

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,20 +3,26 @@ import {CommonModule} from '@angular/common';
 import {HomeComponent} from './pages/home/home.component';
 import {TranslateModule} from '@ngx-translate/core';
 import { LanguageSwitchComponent } from './components/language-switch/language-switch.component';
+import { AboutComponent } from './pages/about/about.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import {RouterModule} from '@angular/router';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    LanguageSwitchComponent
+    LanguageSwitchComponent,
+    AboutComponent,
+    PageNotFoundComponent
   ],
   exports: [
     HomeComponent
   ],
-  imports: [
-    CommonModule,
-    TranslateModule
-  ]
+    imports: [
+        CommonModule,
+        TranslateModule,
+        RouterModule
+    ]
 })
 export class HomeModule {
 }
